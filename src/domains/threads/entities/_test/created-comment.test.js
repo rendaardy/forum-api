f0895cc00b33300bdc4804e1e7c2ccd960702364
Marenda S.A <renda_ardy@tutanota.com>
2022-12-24 16:09:12 +0800

    feat(domains/threads): add created-comment entity

---
+++ src/domains/threads/entities/_test/created-comment.test.js
@@ -0,0 +1,38 @@
+import {describe, it, expect} from '@jest/globals';
+
+import {CreatedComment} from '../created-comment.js';
+
+describe('A CreatedComment entity', () => {
+	it('should throw an error when the payload doesn\'t contain the required properties', () => {
+		const payload = {
+			id: 'comment-123',
+			content: 'a comment',
+		};
+
+		expect(() => new CreatedComment(payload)).toThrowError('CREATED_COMMENT.NOT_MEET_REQUIRED_PROPERTIES');
+	});
+
+	it('should throw an error when the payload has type mismatch', () => {
+		const payload = {
+			id: false,
+			content: 123,
+			owner: '1234',
+		};
+
+		expect(() => new CreatedComment(payload)).toThrowError('CREATED_COMMENT.TYPE_MISMATCH');
+	});
+
+	it('should create \'CreatedComment\' object successfully', () => {
+		const payload = {
+			id: 'comment-123',
+			content: 'a comment',
+			owner: 'user-123',
+		};
+
+		const {id, content, owner} = new CreatedComment(payload);
+
+		expect(id).toEqual(payload.id);
+		expect(content).toEqual(payload.content);
+		expect(owner).toEqual(payload.owner);
+	});
+});
---
+++ src/domains/threads/entities/created-comment.js
@@ -0,0 +1,38 @@
+/**
+ * @typedef {object} Payload
+ * @property {unknown} [id]
+ * @property {unknown} [content]
+ * @property {unknown} [owner]
+ */
+
+export class CreatedComment {
+	id;
+	content;
+	owner;
+
+	/**
+   * @param {Payload} payload
+   */
+	constructor(payload) {
+		const {id, content, owner} = this.#validatePayload(payload);
+
+		this.id = id;
+		this.content = content;
+		this.owner = owner;
+	}
+
+	/**
+   * @param {Payload} payload
+   */
+	#validatePayload({id, content, owner}) {
+		if (id === undefined || content === undefined || owner === undefined) {
+			throw new Error('CREATED_COMMENT.NOT_MEET_REQUIRED_PROPERTIES');
+		}
+
+		if (typeof id !== 'string' || typeof content !== 'string' || typeof owner !== 'string') {
+			throw new Error('CREATED_COMMENT.TYPE_MISMATCH');
+		}
+
+		return {id, content, owner};
+	}
+}
